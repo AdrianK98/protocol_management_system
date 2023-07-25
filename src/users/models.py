b44dc799decc6bae78379ac6a207389735f545bf
AdrianK98 <adrian.k.koczur@gmail.com>
2023-07-25 11:21:59 +0200

    User model change

--- src/users/models.py
+++ src/users/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 #I SHOULD MOVE IT TO ANOTHER APP
 class Employee(models.Model):
+    user_login=models.CharField('Login',max_length=200,blank=True)
     user_name=models.CharField('Imię Pracownika',max_length=200)
     user_surname=models.CharField('Nazwisko Pracownika',max_length=200)
+    user_email=models.CharField('Email',max_length=200,blank=True)
     user_department=models.CharField('Dział',max_length=200,blank=True)
     user_location=models.CharField('Lokalizacja',max_length=200, blank=True)
 
